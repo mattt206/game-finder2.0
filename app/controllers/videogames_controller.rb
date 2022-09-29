09714e60cfc8031fa57ba8970d73b75bfed35f32
mateo holguin <mattt206@gmail.com>
2022-09-29 00:21:32 -0500

    videogame controller ready

--- app/controllers/videogames_controller.rb
+++ app/controllers/videogames_controller.rb
@@ -1,13 +1,23 @@
 class VideogamesController < ApplicationController
 
   skip_before_action :authenticate_user!, only: [:show]
+  before_action :set_videogames, only: %i[show edit update destroy]
+
+  def index
+    @videogames = Videogame.all
+  end
 
   def new
     @videogame = Videogame.new
   end
 
   def create
-
+    @videogame = Videogame.new(videogame_params)
+    if @videogame.save
+      redirect_to users_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def show
@@ -17,17 +27,21 @@ class VideogamesController < ApplicationController
   end
 
   def update
+    @videogame.update(videogame_params)
   end
 
   def destroy
-  end
-
-  def delete
+    @videogame.destroy
+    redirect_to home_path, status: :see_other
   end
 
   private
 
-  def set_params
+  def set_videogames
+    @videogame = @videogame.find(params[:id])
   end
 
+  def videogame_params
+    params.require(:videogame).permit(:user_id, :title, :price, :condition, :description, :platform, :status, :photo)
+  end
 end
