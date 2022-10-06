0f39fdbd07c9dad337b4237a7f74dd8bccdeff53
mateo holguin <mattt206@gmail.com>
2022-10-05 21:24:17 -0500

    avatar ready

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
 
     # For additional in app/views/devise/registrations/edit.html.erb
-    devise_parameter_sanitizer.permit(:account_update, keys: %i[avatar username email])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[profilepicture username email])
   end
 
 end
